6b9f1cfc07fe8c70aed42d75fff99d8b8c493b12
Amponsah-clinton <88673672+Amponsah-clinton@users.noreply.github.com>
2022-10-21 19:13:06 +0000

    Add files via upload

---
+++ python project 2/python project 2.py
@@ -0,0 +1,24 @@
+print("Hello, Welcome to G1 Madlib game") 
+print()
+print("It was a _1_, cold November day. I woke up to the _2_ smell of _3_ roasting in the _4_downstairs. I_5_ down the stairs to see if I could help _6_ the dinner. My mom said,\" See if _7_ needs a fresh _8_.\" So I relative\'s name carried a tray of glasses full of _9_ into a liquid the _10_ room. When I got there, I couldn\'t believe my _11_ ! There were __12__  __13__ on the _14_ ! ")
+
+# taking users input to complete the madlib game
+prompt_1 = input("Enter an adjective for number 1")
+prompt_2 = input("Enter an adjective for number  2")
+prompt_3 = input("Enter a type of bird for number  3")
+prompt_4 = input("Enter a room in a house for number 4")
+prompt_5 = input("Enter a verb in the past tense for number  5")
+prompt_6 = input("Enter a verb for number  6")
+prompt_7 = input("Enter a relative's name for number 7")
+prompt_8 = input("Enter a noun for number 8")
+prompt_9 = input("Enter a liquid for number 9")
+prompt_10 = input("Enter a verb ending in ing for number 10")
+prompt_11 = input("Enter a part of the body plural for number 11")
+prompt_12 = input("Enter a plural noun for number 12")
+prompt_13 = input("Enter a verb ending in ing for number 13")
+prompt_14 = input("Enter a noun for number 14")
+
+
+#the output plus users data
+print()
+print(f"It was a {prompt_1}, cold November day. I woke up to the {prompt_2} smell of {prompt_3} roasting in the {prompt_4} downstairs. I {prompt_5} down the stairs to see if I could help {prompt_6} the dinner. My mom said,\" See if {prompt_7} needs a fresh {prompt_8}.\" So I relative\'s name carried a tray of glasses full of {prompt_9} into a liquid the {prompt_10} room. When I got there, I couldn\'t believe my {prompt_11} ! There were {prompt_12}  {prompt_13} on the {prompt_14} ! ")
